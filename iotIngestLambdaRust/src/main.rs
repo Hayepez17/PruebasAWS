281472b5cf0968a2678fef3ece41c2265b5a0c85
Hayepez17 <hayepez17@gmail.com>
2025-04-25 19:06:59 -0400

    create HashMap for insert query

--- iotIngestLambdaRust/src/main.rs
+++ iotIngestLambdaRust/src/main.rs
@@ -1,7 +1,7 @@
 #![allow(unused)]
 
 use iotIngestLambdaRust::{
-    utilities::{SensorDeviceData, SensorDevicesDatabase},
+    utilities::{SensorDeviceData, SensorDevicesSettings},
     *,
 };
 
@@ -42,8 +42,10 @@ pub async fn function_handler(
 ) -> Result<(), Error> {
     let mut mac_addresses: HashSet<String> = HashSet::new();
 
+    let mut sensor_maps = Vec::new();
+
     for record in event.payload.records {
-        let data = &record.body;
+        let data = record.body;
 
         mac_addresses.insert(data.mac.clone());
 
@@ -51,6 +53,9 @@ pub async fn function_handler(
             println!("Empty MAC address found in the data: {:?}", data);
             continue;
         }
+
+        let sensor_map = SensorDeviceData::generate_sensor_map(data);
+        sensor_maps.push(sensor_map);
     }
 
     let unique_macs: Vec<String> = mac_addresses.into_iter().collect();
@@ -71,22 +76,24 @@ WHERE dl.status != 0 AND d.mac_address IN ({});"#,
         placeholders
     );
 
-    let mut select_query = sqlx::query_as::<_, SensorDevicesDatabase>(&query_devices);
+    let mut select_query = sqlx::query_as::<_, SensorDevicesSettings>(&query_devices);
 
     // Vincular las direcciones MAC
     for mac in unique_macs.iter() {
         select_query = select_query.bind(mac);
     }
 
-    let sensor_devices: Vec<SensorDevicesDatabase> = select_query.fetch_all(&pool).await?;
+    let sensor_devices: Vec<SensorDevicesSettings> = select_query.fetch_all(&pool).await?;
 
     println!("Unique MACs: {:?}", unique_macs);
 
-    let mut count = 0;
-    for sensor_device in sensor_devices {
-        count += 1;
-        println!("Row #{count}: {:?} \n", sensor_device);
+    for (i, sensor_device) in sensor_devices.iter().enumerate() {
+        println!("Row #{}: {:?} \n", i + 1, sensor_device);
     }
 
-    Ok(())
+for (index, sensor_map) in sensor_maps.iter().enumerate() {
+    println!("Sensor map #{}: {:?} \n", index + 1, sensor_map);
+}
+
+Ok(())
 }
--- iotIngestLambdaRust/src/utilities.rs
+++ iotIngestLambdaRust/src/utilities.rs
@@ -1,9 +1,11 @@
+use std::collections::HashMap;
+
 use chrono::NaiveDateTime;
 use sqlx::FromRow;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, FromRow)]
-pub struct SensorDevicesDatabase {
+pub struct SensorDevicesSettings {
     pub mac_address: String,
     pub model: String,
     pub location_name: String,
@@ -33,6 +35,15 @@ pub struct SensorDeviceData {
 }
 
 #[derive(Debug, Serialize, Deserialize)]
+pub struct SensorDeviceRecive {
+    pub sensor_id: String, // "ds18@<sn>" || "<mac>@<byte_array>" || "<mac>@batery"
+    pub model_mac: String, // "<model>@<mac>"
+    pub value: String,
+    pub ip: String,
+    pub time_stamp: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
 pub struct TempSensorFields {
     pub number: u8,
     pub value: Vec<f64>,
@@ -47,4 +58,84 @@ pub struct ModbusRemoteFields {
     pub funct: Vec<u8>,
     pub bytes: Vec<u8>,
     pub addr: Vec<u16>,
+}
+
+impl SensorDeviceRecive {
+    pub fn new(sensor_id: String, model_mac: String, value: String, ip: String, time_stamp: String) -> Self {
+        SensorDeviceRecive {
+            sensor_id,
+            model_mac,
+            value,
+            ip,
+            time_stamp,
+        }
+    }
+}
+
+impl SensorDeviceData {
+    pub fn generate_sensor_map(sensor_data: SensorDeviceData) -> HashMap<String, Vec<SensorDeviceRecive>>{
+
+        let mut sensor_map: HashMap<String, Vec<SensorDeviceRecive>> = HashMap::new();
+
+    // Procesar el campo 'temp'
+    if let Some(temp_fields) = sensor_data.temp {
+        for sn in temp_fields.sn {
+            let sensor_id = format!("ds18@{}", sn);
+            let model_mac = format!("{}@{}", sensor_data.model, sensor_data.mac);
+            let value = temp_fields.value.get(0).map_or_else(|| "N/A".to_string(), |v| v.to_string());
+
+            let entry = SensorDeviceRecive {
+                sensor_id: sensor_id.clone(),
+                model_mac: model_mac.clone(),
+                value,
+                ip: sensor_data.ip.clone(),
+                time_stamp: sensor_data.time_stamp.clone(),
+            };
+
+            sensor_map.entry(sensor_id).or_insert_with(Vec::new).push(entry);
+        }
+    }
+
+    // Procesar el campo 'modbus'
+    if let Some(modbus_fields) = sensor_data.modbus {
+        for (i, value) in modbus_fields.value.iter().enumerate() {
+            let sensor_id = format!(
+                "{}@{:02X}{:02X}{:04X}{:02X}",
+                sensor_data.mac,
+                modbus_fields.slave[i],
+                modbus_fields.funct[i],
+                modbus_fields.addr[i],
+                modbus_fields.bytes[i]
+            );
+
+            let model_mac = format!("{}@{}", sensor_data.model, sensor_data.mac);
+
+            let entry = SensorDeviceRecive::new(
+                sensor_id.clone(),
+                model_mac.clone(),
+                value.clone(),
+                sensor_data.ip.clone(),
+                sensor_data.time_stamp.clone(),
+            );
+
+            sensor_map.entry(sensor_id).or_insert_with(Vec::new).push(entry);
+        }
+
+        // Procesar el campo 'batery'
+
+        let sensor_id = format!("{}@batery", sensor_data.mac);
+        let model_mac = format!("{}@{}", sensor_data.model, sensor_data.mac);
+        let value = sensor_data.batery.to_string();
+        let entry = SensorDeviceRecive::new(
+            sensor_id.clone(),
+            model_mac.clone(),
+            value.clone(),
+            sensor_data.ip.clone(),
+            sensor_data.time_stamp.clone(),
+        );
+        sensor_map.entry(sensor_id).or_insert_with(Vec::new).push(entry);
+    }
+
+    sensor_map
+    }
 }
